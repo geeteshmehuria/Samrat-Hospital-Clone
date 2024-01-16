5a4084f7a28b6b48e5a79371e66603f5bc1dc590
geeteshmehuria <geeteshmaihuria@gmail.com>
2024-01-16 11:13:31 +0530

    data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Navbar = () => {
+const Home = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Home
