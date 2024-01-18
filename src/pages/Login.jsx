db8f501bcb39a93598c485913f7ec7b3f03fee0d
VishnurajKarockal <vishnurajkarockal@gmail.com>
2024-01-18 23:03:02 +0530

    Vishnuraj K R day 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,104 @@
-import React from "react";
+import { FormControl, FormLabel, Input, FormErrorMessage, InputGroup, InputRightElement, Button, Box, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from 'axios';
+import { useToast } from '@chakra-ui/react'
 
 const Login = () => {
-  return <div></div>;
+  const [show, setShow] = React.useState(false)
+  const handleClick = () => setShow(!show)
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [logintry,setLoginTry] = useState(false);
+  const [login,setLogin] = useState(false);
+  const toast = useToast()
+  console.log(email);
+  var isCredentialCorrect = false;
+  const AccountCreateToast  = () => {
+    
+    const examplePromise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(200), 2000)
+    })
+    return (
+      toast.promise(examplePromise, {
+        success: { title: 'Login Successfull!..', description: 'Welcome Back!',isClosable: true },
+        error: { title: 'Promise rejected', description: 'Something wrong' },
+        loading: { title: 'Checking Credentials', description: 'Please wait' },
+        
+      })
+    )
+  }
+
+  const accountCreateFail = () => {
+    
+    return(
+      toast({
+        title: 'Invalid Credentials.',
+        description: "Check all fields ",
+        status: 'warning',
+        duration: 1000,
+        isClosable: true,
+      })
+    )
+  }
+
+  const fetchUserData = async () => {
+    setLoginTry(true);
+    try {
+      const res = await axios.get(`https://mock-api-39gi.onrender.com/user`);
+      const data = res.data;
+      // let isCredentialCorrect = false;
+      console.log("fetchData",data);
+      for (const item of data) {
+        if (item.email === email && item.password === password) {
+          isCredentialCorrect = true;
+          setLogin(true);
+          AccountCreateToast();
+          break; 
+        }
+      }
+  
+      if (!isCredentialCorrect) {
+        accountCreateFail();
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+    console.log("ligonCred",isCredentialCorrect);
+  };
+
+  
+  return (
+    <Box margin={"auto"} color={"green"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"} padding={"30px"} width={{ base: "90%", sm: "60%", md: "60%", lg: "50%", xl: "40%" }}>
+      <Text textAlign={'center'} fontSize={"1.7rem"}>SIGN IN</Text>
+
+      <FormControl isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder='Email'
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <FormLabel>Password</FormLabel>
+        <InputGroup size='md'>
+          <Input
+            pr='4.5rem'
+            type={show ? 'text' : 'password'}
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width='4.5rem'>
+            <Button h='1.75rem' size='sm' onClick={handleClick}>
+              {show ? 'Hide' : 'Show'}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Button colorScheme='blue' width='full' marginTop={"15px"} onClick={fetchUserData}>SIGN IN</Button>
+        {logintry && !login ? <h1 style={{"color":"red"}}>Credentials Not Found. Please <a href="./SignUp.jsx" style={{"color":"blue"}}>Sign Up</a></h1> : <></>}
+      </FormControl>
+    </Box>
+  );
 };
 
 export default Login;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,147 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Checkbox, Input, InputGroup, InputLeftAddon, InputRightElement, Select, Text, Toast } from '@chakra-ui/react';
+import { FormControl, FormLabel } from '@chakra-ui/react';
+import axios from "axios" ;
+import { useToast } from '@chakra-ui/react'
 const Sign = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const [Fname, setFname] = useState("");
+  const [Lname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [number, setNumber] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [pass1, setPass1] = useState("");
+  const [pass2, setPass2] = useState("");
+  const [passwordChange, setPasswordChange] = useState(true);
+  const [allFeild,setAllfield] = useState(false);
+  const toast = useToast()
+  const obj = {
+    "firstName": Fname,
+    "lastName": Lname,
+    "email": email,
+    "mobile": number,
+    "dateOfBirth": dob,
+    "gender": gender,
+    "password": pass2,
+  }
 
-export default Sign
+  const AccountCreateToast  = () => {
+    
+    const examplePromise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(200), 2000)
+    })
+    return (
+      toast.promise(examplePromise, {
+        success: { title: 'Account Successfully Created', description: 'Welcome!' },
+        error: { title: 'Promise rejected', description: 'Something wrong' },
+        loading: { title: 'Creating Account', description: 'Please wait' },
+      })
+    )
+  }
+  
+  const accountCreateFail = () => {
+    
+    return(
+      toast({
+        title: 'Invalid Credentials.',
+        description: "Complete all fields ",
+        status: 'warning',
+        duration: 1000,
+        isClosable: true,
+      })
+    )
+  }
+
+  const allFieldHandler = (e) => {
+    const AddUser = async () => {
+      const res = await axios.post(`https://mock-api-39gi.onrender.com/user`,obj);
+    }
+    
+    e.preventDefault();
+    if (Fname.length && Lname.length && email.length && number.length && dob.length && gender.length && pass1.length && pass2.length && passwordChange && document.getElementById("SignUpCheckbox1")?.checked && document.getElementById("SignUpCheckbox2")?.checked) {
+      setAllfield(true);
+      console.log("vishnurajkr",allFeild);
+    }
+    if(allFeild){
+      AddUser();
+      AccountCreateToast();
+    }
+    else{
+      accountCreateFail()
+    }
+  }
+  
+  
+
+  const confirmPassChange = (e) => {
+    const newPassword = e.target.value;
+    setPass2(newPassword);
+    if(pass1 === newPassword){
+      setPasswordChange(true);
+    }
+    
+    
+  };
+
+
+  
+  return (
+    <Box margin={"auto"} color={"green"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"} padding={"30px"} width={{ base: "90%", sm: "60%", md: "60%", lg: "50%", xl: "40%" }}>
+      <Text textAlign={'center'} fontSize={"1.7rem"}>CREATE ACCOUNT</Text>
+      <form >
+        <FormControl isRequired>
+          <FormLabel htmlFor="Fname">First name</FormLabel>
+          <Input id="Fname" placeholder='First name' onChange={(e) => setFname(e.target.value)} />
+          <FormLabel htmlFor="Lname">Last name</FormLabel>
+          <Input id="Lname" placeholder='Last name' onChange={(e) => setLname(e.target.value)} />
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input id="email" placeholder='Email' type='email' onChange={(e) => setEmail(e.target.value)} />
+          <FormLabel htmlFor="number">Mobile Number</FormLabel>
+          <InputGroup>
+            <InputLeftAddon>
+              +91-
+            </InputLeftAddon>
+            <Input id="number" type='tel' placeholder='phone number' onChange={(e) => setNumber(e.target.value)} />
+          </InputGroup>
+          <FormLabel htmlFor="dob">Birth Date</FormLabel>
+          <Input id="dob" placeholder="Select Date and Time" size="md" type="date" onChange={(e) => setDob(e.target.value)} />
+          <FormLabel htmlFor="gender">Gender</FormLabel>
+          <Select id="gender" placeholder='Gender' onChange={(e) => setGender(e.target.value)}>
+            <option value='Male'>Male</option>
+            <option value='Female'>Female</option>
+          </Select>
+          <FormLabel htmlFor="pass1">Password</FormLabel>
+          <InputGroup size='md'>
+            <Input id="pass1" pr='4.5rem' type={show ? 'text' : 'password'} placeholder='Enter password' onChange={(e) => {
+              setPass1(e.target.value) ;
+              setPasswordChange(false);
+            }} />
+            <InputRightElement width='4.5rem'>
+              <Button h='1.75rem' size='sm' onClick={handleClick}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <FormLabel htmlFor="pass2" borderColor={passwordChange ? "inherit" : "red"}>Confirm Password</FormLabel>
+          <Input
+            id="pass2"
+            placeholder='Password'
+            type='password'
+            onChange={confirmPassChange}
+            borderColor={passwordChange ? "inherit" : "red"}
+          />
+          {passwordChange === false && <Text color="red">Passwords do not match.</Text>}
+          <Checkbox id='SignUpCheckbox1'>Subscribe to our newsletters</Checkbox><br />
+          <Checkbox id='SignUpCheckbox2'>I have read and agree to the </Checkbox>
+          <Text as={"span"} textDecoration={"underline"} color={"blue"} > <a href='https://getbootstrap.com/docs/5.3/getting-started/vite/'>terms and conditions</a></Text>
+          <br />
+          <Button colorScheme='blue' width='full' marginTop={"15px"} onClick={allFieldHandler}>Create New Account</Button>
+        </FormControl>
+      </form>
+    </Box>
+  );
+};
+
+export default Sign;
